330ae3dbc6840de97b8d8b4b52283433c9da63f2
Georges Dib <georges.dib@gmail.com>
2021-08-30 23:31:16 +0100

    capital

--- login.ts
+++ login.ts
@@ -20,13 +20,13 @@ connection.query(
     USE nodelogin
     
     CREATE TABLE IF NOT EXISTS accounts (
-      id int,
-      username varchar(50),
-      password varchar(255),
-      email varchar(100)
+      Id int,
+      Username varchar(50),
+      Password varchar(255),
+      Email varchar(100)
     ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
     
-    INSERT INTO \`accounts\` (\`id\`, \`username\`, \`password\`, \`email\`) VALUES (1, 'test', 'test', 'test@test.com');
+    INSERT INTO accounts (Id, Username, Password, Email) VALUES (1, 'test', 'test', 'test@test.com');
     
     ALTER TABLE \`accounts\` ADD PRIMARY KEY (\`id\`);
     ALTER TABLE \`accounts\` MODIFY \`id\` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;`
