0c92e3b7d288f42c0ec078d13a49beb0fe727dbb
Georges Dib <georges.dib@gmail.com>
2021-08-30 23:25:06 +0100

    Change syntax

--- login.ts
+++ login.ts
@@ -20,7 +20,7 @@ connection.query(
     USE \`nodelogin\`
     
     CREATE TABLE IF NOT EXISTS \`accounts\` (
-      \`id\` int(11) NOT NULL,
+      \`id\` int NOT NULL,
       \`username\` varchar(50) NOT NULL,
       \`password\` varchar(255) NOT NULL,
       \`email\` varchar(100) NOT NULL
