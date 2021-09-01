0425d32c3e286911e414a02c269e37a0b0ef1f76
Georges Dib <georges.dib@gmail.com>
2021-09-01 23:06:53 +0100

    Fixing some login stuff

--- login.ts
+++ login.ts
@@ -4,7 +4,7 @@ const session = require("express-session");
 const bodyParser = require("body-parser");
 const path = require("path");
 
-require("dotenv").config({path: "./.env"});
+require("dotenv").config({ path: "./.env" });
 
 const PORT = process.env.PORT || 5000;
 
@@ -16,14 +16,17 @@ const connection = mysql.createConnection({
   database: process.env.DATABASE,
 });
 
-console.log(connection);
-
 // Initialize DB
-const createTable = `CREATE TABLE IF NOT EXISTS \`accounts\` (
-        \`id\` int(11) NOT NULL,
+const deleteTable = "DROP TABLE accounts";
+connection.query(deleteTable, function (err, results, fields) {
+  if (err) {
+    console.log(err.message);
+  }
+});
+
+const createTable = `CREATE TABLE \`accounts\` (
         \`username\` varchar(50) NOT NULL,
-        \`password\` varchar(255) NOT NULL,
-        \`email\` varchar(100) NOT NULL
+        \`password\` varchar(255) NOT NULL
       ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;`;
 
 connection.query(createTable, function (err, results, fields) {
@@ -37,9 +40,14 @@ const app = express();
 
 app.use(
   session({
-    secret: "coursera_secret",
+    secret: "kjsbfkjsbjkvbwijfb",
     resave: true,
-    saveUninitialized: true,
+    saveUninitialized: false,
+    cookie: {
+      //secure: true,
+      maxAge: 3600000, // 1hour
+      sameSite: true,
+    },
   })
 );
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -72,6 +80,48 @@ app.post("/auth", function (request, response) {
   } else {
     response.send("Please enter Username and Password!");
     response.end();
+  }
+});
+
+// Handle register
+app.post("/register", function (request, response) {
+  const username = request.body.username;
+  // Validate username contains characters only
+  const regex = /^[a-zA-Z]+$/;
+  const onlyChar = regex.test(username);
+  const password = request.body.password;
+  if (!(username && password && username.length >= 1 && password.length >= 1)) {
+    response.send("Please enter Username and Password!");
+    response.end();
+  } else if (!onlyChar) {
+    response.send("Username can only contain characters");
+    response.end();
+  } else {
+    // Check if the username exists already
+    connection.query(
+      "SELECT * FROM accounts WHERE username = ?",
+      [username],
+      function (error, results) {
+        if (results.length > 0) {
+          response.send("Username already exists, maybe you meant to login?");
+          response.end();
+        } else {
+          let query =
+            "INSERT INTO `accounts` (`username`, `password`) VALUES ('";
+          query += username + "', '" + password + "');";
+          connection.query(query, function (error, results) {
+            if (error) {
+              response.send("Error: " + error);
+            } else if (results) {
+              response.send("Username registered");
+            } else {
+              response.send("Failed to register!");
+            }
+            response.end();
+          });
+        }
+      }
+    );
   }
 });
 
